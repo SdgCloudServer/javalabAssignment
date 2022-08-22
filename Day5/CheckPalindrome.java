7d4c23a7cbc762affa848d068c29fe4c731e0d7a
SdgCloudServer <sdgcloudserver@gmail.com>
2022-08-22 11:40:32 +0530

    added Palindrome

--- Day5/CheckPalindrome.java
+++ Day5/CheckPalindrome.java
@@ -3,20 +3,20 @@ class Palindrome
 {
 	public static void main(String args[]) throws IOException
 	{
-		BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter The String:");
-		String str = br.readLine ();
+		String str = br.readLine();
 		String temp = str;
-		StringBuffer sb = new StringBuffer (str);
-		sb.reverse ();
-		str = sb.toString ();
-		if (temp.equalsIgnoreCase (str))
+		StringBuffer sb = new StringBuffer(str);
+		sb.reverse();
+		str = sb.toString();
+		if(temp.equalsIgnoreCase(str))
 		{
-			System.out.println (temp + " Is Palindrome.");
+			System.out.println(temp + " Is Palindrome.");
 		}
 		else
 		{
-			System.out.println (temp + " Is Not Palindrome.");
+			System.out.println(temp + " Is Not Palindrome.");
 		}
 	}
 }
