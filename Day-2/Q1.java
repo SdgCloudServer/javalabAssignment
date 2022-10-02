3533768aaa6e58c0f6eb2bbdb6c80c2160dde459
Tech Optimist <soumwadeepguha@gmail.com>
2022-10-02 09:08:32 +0530

    day 2

---
+++ Day-2/Q1.java
@@ -0,0 +1,23 @@
+/*WAP To Display The Following Pattern Using continue statement:
+**
+**
+**
+**
+*/
+class Q1
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<=4;i++)
+        {
+            if(i==1)
+            {
+                continue;
+            }
+            else
+            {
+                System.out.println("**");
+            }
+        }
+    }
+}
